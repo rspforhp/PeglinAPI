3657d3c63565a1fcf88dbdda71850b0f27e8c004
rspforhp <petkermihail@gmail.com>
2022-04-27 19:37:35 +0500

    Base orb builder done, next are patches

---
+++ OrbBuilder.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using UnityEngine;
+
+namespace API
+{
+    public class OrbBuilder
+    {
+        public OrbBuilder()
+        {
+            _info = new OrbInfo(this);
+        }
+
+        private OrbInfo _info;
+
+        public class OrbInfo
+        {
+            public OrbInfo(OrbBuilder instance) : base()
+            {
+                if (instance == null )
+                {
+                    throw (new Exception("This is not how you supposed to instantiate this, use OrbBuilder instead"));
+                }
+            }
+
+            public string name;
+            public float damage;
+            public float critDamage;
+            public OrbInfo nextLevel;
+        }
+
+        public OrbBuilder WithName(string name)
+        {
+            _info.name = name;
+            return this;
+        }
+        public OrbBuilder WithDamage(int dmg)
+        {
+            _info.damage = dmg;
+            return this;
+        }
+        public OrbBuilder WithCriticalDamage(int dmg)
+        {
+            _info.critDamage = dmg;
+            return this;
+        }
+        public OrbBuilder WithNextLevel(OrbInfo info)
+        {
+            _info.nextLevel = info;
+            return this;
+        }
+
+        public (OrbInfo, GameObject) Build()
+        {
+            GameObject gameObject = new GameObject();
+            
+            Plugin.allOrbs.Add(_info, gameObject);
+            return (_info,gameObject);
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace API
+{
+    [BepInPlugin(PluginGuid, PluginName, PluginVersion)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private const string PluginGuid = "kopie.peglin.api";
+        private const string PluginName = "Peglin_API";
+        private const string PluginVersion = "0.0.1";
+
+        private void Awake()
+        {
+        }
+
+        internal static Dictionary<OrbBuilder.OrbInfo, GameObject> allOrbs = new Dictionary<OrbBuilder.OrbInfo, GameObject>();
+
+
+        [HarmonyPatch(typeof(PopulateSuggestionOrbs), nameof(Awake))]
+        public static class AddNewOrbsToPools
+        {
+            [HarmonyPrefix]
+            public static void Prefix(ref PopulateSuggestionOrbs __instance)
+            {
+                
+            }
+        }
+    }
+}
